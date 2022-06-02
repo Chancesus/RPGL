0646042dd55680709c621791104206cc1950abad
Chancesus <chancekgsmith@gmail.com>
2022-06-02 16:38:46 -0700

    Started building dialog controller

---
+++ RPGL Project/Assets/Scripts/UI/DialogController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+using System;
+using System.Text;
+
+public class DialogController : MonoBehaviour
+{
+    [SerializeField] TextAsset _dialog;
+    [SerializeField] TMP_Text _storytText;
+    [SerializeField] Button[] _choiceButtons;
+
+    Story _story;
+    [ContextMenu("Start Dialog")]
+    public void StartDialog()
+    {
+        _story = new Story(_dialog.text);
+        RefreshView();
+    }
+
+     void RefreshView()
+    {
+        StringBuilder storyTextBuilder = new StringBuilder();
+        while (_story.canContinue)
+        {
+            storyTextBuilder.AppendLine(_story.Continue());
+
+            _storytText.SetText(storyTextBuilder);
+
+            for (int i = 0; i < _choiceButtons.Length; i++)
+            {
+                var button = _choiceButtons[i];
+                //Turns on and off choices 
+                button.gameObject.SetActive(i < _story.currentChoices.Count);
+                button.onClick.RemoveAllListeners();
+
+                if (i < _story.currentChoices.Count)
+                {
+                    var choice = _story.currentChoices[i];
+                    button.GetComponentInChildren<TMP_Text>().SetText(choice.text);
+                    button.onClick.AddListener(() =>
+                    {
+                        _story.ChooseChoiceIndex(choice.index);
+                        RefreshView();
+                    });
+                }
+            }
+        }
+    }
+}
