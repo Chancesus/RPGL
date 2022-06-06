e08fdf7ed072c20fd94d5835e9f0d399c8fcdc26
Chancesus <chancekgsmith@gmail.com>
2022-06-06 16:28:17 -0700

    Added Dialog/Event integration

--- RPGL Project/Assets/Scripts/GameEvents/GameEvent.cs
+++ RPGL Project/Assets/Scripts/GameEvents/GameEvent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,39 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game Event")]
 public class GameEvent : ScriptableObject
 {
+    private static HashSet<GameEvent> _listenedEvents = new HashSet<GameEvent>();
+
     HashSet<GameEventListener> _gameEventListeners = new HashSet<GameEventListener>();
 
-    public void Register(GameEventListener gameEventListener) => _gameEventListeners.Add(gameEventListener);
+    public void Register(GameEventListener gameEventListener)
+    {
+        _gameEventListeners.Add(gameEventListener);
+        _listenedEvents.Add(this);
+    }
 
-    public void Deregister(GameEventListener gameEventListener) => _gameEventListeners.Remove(gameEventListener);
-    
-    
+
+    public void Deregister(GameEventListener gameEventListener)
+    {
+        _gameEventListeners.Remove(gameEventListener);
+        if (_gameEventListeners.Count == 0)
+            _listenedEvents.Remove(this);
+    }
+
     [ContextMenu("Invoke")]
     public void Invoke()
     {
         foreach (var gameEventListener in _gameEventListeners)
         {
             gameEventListener.RaiseEvent();
+        }
+    }
+
+    public static void RaiseEvent(string eventName)
+    {
+      foreach (var gameEvent in _listenedEvents)
+        {
+            if (gameEvent.name == eventName)
+            gameEvent.Invoke();
         }
     }
 }
--- RPGL Project/Assets/Scripts/UI/DialogController.cs
+++ RPGL Project/Assets/Scripts/UI/DialogController.cs
@@ -63,13 +63,12 @@ public class DialogController : MonoBehaviour
         foreach (var tag in _story.currentTags)
         {
             Debug.Log(tag);
-            if (tag == "OpenDoor")
-                OpenDoor();
+            if (tag.StartsWith("E."))
+            {
+                string eventName = tag.Remove(0, 2);
+                GameEvent.RaiseEvent(eventName);
+            }
+               
         }
-    }
-
-     void OpenDoor()
-    {
-        _animator.SetTrigger("Open");
     }
 }
