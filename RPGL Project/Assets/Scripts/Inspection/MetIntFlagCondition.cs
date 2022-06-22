6a240a65956e84e87d50af0403ead565093573f8
Chancesus <chancekgsmith@gmail.com>
2022-06-21 21:00:56 -0700

    Int Flag Condition
    Added ability to use Int Flag as a condition for quest

---
+++ RPGL Project/Assets/Scripts/Inspection/MetIntFlagCondition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MetIntFlagCondition : MonoBehaviour, IMetInspectedCondition
+{
+    [SerializeField] IntGameFlag _requiredIntFlag;
+    [SerializeField] int _requiredValue = 2;
+
+    public bool Met()
+    {
+        if (_requiredIntFlag.Value >= _requiredValue)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
